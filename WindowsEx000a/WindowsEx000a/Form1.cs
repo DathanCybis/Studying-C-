165201d14191c648dd69b383819ec51c0f51ea98
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-03-10 22:21:10 -0300

    DIA 001

--- WindowsEx000a/WindowsEx000a/Form1.cs
+++ WindowsEx000a/WindowsEx000a/Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsEx000a
         {
             lblMsg.Text = "Hello, World!";
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
