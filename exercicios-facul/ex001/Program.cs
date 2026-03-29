c354fc6370391a0dfeb80f875c785e58cb9b2bcf
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-03-29 19:17:24 -0300

    ex002

--- exercicios-facul/ex001/Program.cs
+++ exercicios-facul/ex001/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int idade1 = 15;
+		int idade2 = 20;
+		int idade3 = 21;
+		
+		double media = (idade1 + idade2 + idade3) / 3.0;
+		
+		Console.WriteLine("A media das idades é: {0:0.0}", media);
+	}
+}
---
+++ exercicios-facul/ex002/Program.cs
@@ -0,0 +1,3 @@
+﻿int pi = 3;
+		
+Console.WriteLine("O pi é: {0}", pi);
