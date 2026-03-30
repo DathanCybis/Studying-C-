13593464fda1cdb2affa940581f98fb042857910
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-03-30 18:18:14 -0300

    Conceitos POO C#

---
+++ estudos-pessoais/POO1/Program.cs
@@ -0,0 +1,16 @@
+﻿Pessoa obj1 = new Pessoa();
+obj1.Nome = "Ralf";
+obj1.Apresentacao();
+
+Pessoa obj2 = new();
+obj2.Nome = "Stella";
+obj2.Apresentacao();
+
+Pessoa obj3 = new()
+{
+    Nome = "Merlin",
+    Idade = 23
+};
+obj3.Apresentacao();
+string retorno = obj3.VerificarIdade();
+Console.WriteLine(retorno);
